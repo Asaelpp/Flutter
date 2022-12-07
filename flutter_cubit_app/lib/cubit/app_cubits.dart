5da840798f9bebf4ef5f5e0e791acebe84717256
Asael Paulino Pereira <asael_pereira@yahoo.com>
2022-12-07 15:09:37 -0300

    implementação cubit

---
+++ flutter_cubit_app/lib/cubit/app_cubits.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_cubit/services/data_services.dart';
+
+import 'app_cubit_states.dart';
+
+class AppCubits extends Cubit<CubitStates> {
+  AppCubits({required this.data}) : super(InitialState()) {
+    emit(WelcomeState());
+  }
+
+  final DataServices data;
+  late final places;
+
+  Future<void> getData() async {
+    try {
+      emit(LoadingState());
+      places = await data.getInfo();
+      emit(LoadedState(places));
+    } catch (e) {}
+  }
+}
